4d6f9509d887e382c2fbdab8c06e323be8dd4eaa
manoj <mshide@tavisca.com>
2016-09-08 21:00:17 +0530

    window server

---
+++ WCFserviceApplication/Employee/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employee
+    {
+
+        public string Id { set; get; }
+        public string Name { set; get; }
+        public string Date { set; get; }
+        public Gender gender { set; get; }
+
+    }
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ WCFserviceApplication/WindowServiceApplication/MyService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Employee;
+
+namespace WindowServiceApplication
+{
+    public partial class MyService : ServiceBase
+    {
+        ServiceHost host;
+        public MyService()
+        {
+            
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(EmployeeService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+    }
+}
